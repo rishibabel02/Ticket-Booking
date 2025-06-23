54e34975300edf304076e6ea745e97d7a2b7bcf5
Rishi Babel <rishibabel02@gmail.com>
2025-06-23 21:16:09 +0530

    Set up a local DB

--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -1,11 +1,26 @@
 package ticket.booking.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import ticket.booking.entities.User;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class UserBookingService {
     private User user;
+    private List<User> userList;
 
-    public UserBookingService(User user1){
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final String USERS_PATH = "../localDb/users.json";
+
+    public UserBookingService(User user1) throws IOException {
         this.user = user1;
+        File users = new File(USERS_PATH);
+
+//        Here I am deserializing users.json to be used in the future.
+        userList= objectMapper.readValue(users, new TypeReference<List<User>> () {});
     }
 }
